0c465f77cebb3bfc25203288492941c52c1a66c2
Aarjav-Jain <aarjavjain2309@gmail.com>
2021-11-25 22:55:10 +0530

    now adding maps

---
+++ data.js
@@ -0,0 +1,25 @@
+const mines = [
+  {
+    id: 001,
+    name: "Bailadila Iron Ore Mine,Kirandul Complex",
+    geoDetail:
+      "Bailadila Range, is a mountain range rising in the Deccan Plateau about 200 km west of the Eastern Ghats. It has been named 'Bailadila' because it resembles the hump of an ox. It is located near Kirandul town in the Dantewada district of southern Chhattisgarh, India.",
+    location:
+      "Kirandul Complex, South Bastar district, Dantewada, Chhattisgarh",
+    background:
+      "Bailadila-14 mine is the first large scale open cast mechanised iron ore mine in India for which DPR was prepared by NMDC. The mine has the distinction of having unique down-the-hill conveyor system passing through a tunnel to transport iron ore from crushing plant to processing plant.",
+    currentStatus: "Operating",
+    photos: "",
+    oreMined: "Iron",
+    grade: "hematite Ore(66% Fe)",
+    production: "3.4 Million Tonne ROM (in 2019)",
+    totalReserve: "138.29 million tonnes (April, 08)",
+    mineType: "Open Cast",
+    amountOfOreMined: "",
+    usage: "exported from Visakhapatnam port to Japan and South Korea",
+    company: "NMDC",
+    commisionedDate: "April,1968",
+  },
+];
+
+module.exports = mines;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const data = require("./data");
 const app = express();
 const path = require("path");
 
@@ -11,6 +12,17 @@ app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  let mine;
+  for (let d of data) {
+    if (parseInt(id) == d.id) {
+      mine = { ...d };
+    }
+  }
+  res.render("minedetails", { mine });
 });
 
 app.listen(PORT, () => {
