eb51884641ded539b1c6ebf039457f45beb41dee
Aarjav-Jain <aarjavjain2309@gmail.com>
2021-11-26 03:42:35 +0530

    completed minedetails page

--- data.js
+++ data.js
@@ -2,6 +2,8 @@ const mines = [
   {
     id: 001,
     name: "Bailadila Iron Ore Mine,Kirandul Complex",
+    lat: 18.695336361826122,
+    long: 81.22117259277897,
     geoDetail:
       "Bailadila Range, is a mountain range rising in the Deccan Plateau about 200 km west of the Eastern Ghats. It has been named 'Bailadila' because it resembles the hump of an ox. It is located near Kirandul town in the Dantewada district of southern Chhattisgarh, India.",
     location:
@@ -19,6 +21,10 @@ const mines = [
     usage: "exported from Visakhapatnam port to Japan and South Korea",
     company: "NMDC",
     commisionedDate: "April,1968",
+    url: {
+      NMDC: "https://www.nmdc.co.in/KirandulComplex.aspx#:~:text=Bailadila%2D14%20mine%20is%20the,Bailadila%20Deposit%20%2D%2011%2FC",
+      Wikipedia: "https://en.wikipedia.org/wiki/Bailadila_Range",
+    },
   },
 ];
 
