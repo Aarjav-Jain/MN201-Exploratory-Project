849390aba31e3d612ef18743b29268e973f91ce0
Aarjav-Jain <aarjavjain2309@gmail.com>
2021-11-26 22:51:46 +0530

    Added 9 mine entries

--- data.js
+++ data.js
@@ -1,29 +1,225 @@
 const mines = [
   {
     id: 001,
-    name: "Bailadila Iron Ore Mine,Kirandul Complex",
+    Name: "Bailadila Iron Ore Mine,Kirandul Complex",
     lat: 18.695336361826122,
     long: 81.22117259277897,
     geoDetail:
       "Bailadila Range, is a mountain range rising in the Deccan Plateau about 200 km west of the Eastern Ghats. It has been named 'Bailadila' because it resembles the hump of an ox. It is located near Kirandul town in the Dantewada district of southern Chhattisgarh, India.",
-    location:
+    Location:
       "Kirandul Complex, South Bastar district, Dantewada, Chhattisgarh",
     background:
       "Bailadila-14 mine is the first large scale open cast mechanised iron ore mine in India for which DPR was prepared by NMDC. The mine has the distinction of having unique down-the-hill conveyor system passing through a tunnel to transport iron ore from crushing plant to processing plant.",
-    currentStatus: "Operating",
+    "Current Status": "Operating",
     photos: "",
-    oreMined: "Iron",
-    grade: "hematite Ore(66% Fe)",
-    production: "3.4 Million Tonne ROM (in 2019)",
-    totalReserve: "138.29 million tonnes (April, 08)",
-    mineType: "Open Cast",
-    amountOfOreMined: "",
-    usage: "exported from Visakhapatnam port to Japan and South Korea",
-    company: "NMDC",
-    commisionedDate: "April,1968",
+    "Mineral Mined": "Iron",
+    Grade: "hematite Ore(66% Fe)",
+    "Production Capacity": "3.4 Million Tonne ROM (in 2019)",
+    "Total Reserve": "138.29 million tonnes (April, 08)",
+    "Mine Type": "Open Cast",
+    "Amount Of Ore Mined": "",
+    Usage: "exported from Visakhapatnam port to Japan and South Korea",
+    Company: "National Mineral Development Corporation (NMDC Ltd)",
+    "Commisioned Date": "April,1968",
     url: {
-      NMDC: "https://www.nmdc.co.in/KirandulComplex.aspx#:~:text=Bailadila%2D14%20mine%20is%20the,Bailadila%20Deposit%20%2D%2011%2FC",
+      "National Mineral Development Corporation (NMDC Ltd)":
+        "https://www.nmdc.co.in/KirandulComplex.aspx#:~:text=Bailadila%2D14%20mine%20is%20the,Bailadila%20Deposit%20%2D%2011%2FC",
       Wikipedia: "https://en.wikipedia.org/wiki/Bailadila_Range",
+    },
+  },
+  {
+    id: 002,
+    Name: "Diamond Mining Project, Panna",
+    lat: 24.27,
+    long: 80.17,
+    geoDetail:
+      "Panna is a city and a municipality in Panna district in the Indian state of Madhya Pradesh. It is famous for its diamond mines. It is the administrative center of Panna District.   A large group of diamond deposits extends North-East on a branch of the Vindhya Range for 240 kilometres (150 mi) or so, and is known as the Panna group. India was the only source of diamonds in the world for over 3000 years till diamond deposits were located in Brazil and South Africa. Today, NMDC is the only organised producer of diamond in India from its Majhgawan mine at Panna, Madhya Pradesh and continues to carry on the legend and past glory through its untiring efforts.",
+    Location: "Panna District,Madhya Pradesh",
+    background:
+      "According to Valentine Ball, who edited the 1676 'Travels in India' of Jean-Baptiste Tavernier, Tieffenthaler was the first European to visit the mines in 1765 and claimed that the Panna diamonds could not compare in hardness and fire with other locations in India. No really large diamonds have come from this area.The most productive mines were in the 1860s and were found in Sakaria, around 32 kilometres (20 mi) from Panna. Four classifications were given to the Panna diamonds: first, Motichul, clear and brilliant; 2nd, Manik, with a faint orange tint; 3rd, Panna, verging in tint towards green; 4th, Bunsput, sepia coloured. Mines is situated in the interior of Panna district. Diamond mines in Panna are managed under the Diamond Mining Project of National Mineral Development Corporation (NMDC Ltd) of Government of India.",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Diamond",
+    Grade: "kimberlite",
+    "Production Capacity": "84,000 carats per year",
+    "Mine Type": "Open Cast",
+    "Amount Of Ore Mined": "1005064 carats approx.",
+    Usage:
+      "The diamonds unearthed are all collected by the district magistrate of Panna and are auctioned in the month of January.",
+    Company: "National Mineral Development Corporation (NMDC Ltd)",
+    "No Of Employees": "167 ( as on 30/06/2021 )",
+    "Commisioned Date": "1967",
+    url: {
+      "National Mineral Development Corporation (NMDC Ltd)":
+        "https://www.nmdc.co.in/Panna.aspx",
+      "Panna Mine on Wikipedia": "https://en.wikipedia.org/wiki/Panna,_India",
+      "Diamond Mines": "https://en.wikipedia.org/wiki/Diamond_mining_in_India",
+    },
+  },
+  {
+    id: 003,
+    Name: "Jharia Coalfield",
+    lat: 23.75,
+    long: 86.42,
+    geoDetail:
+      "The coal field lies in the Damodar River Valley, and covers about 110 square miles (280 square km), and produces bituminous coal suitable for coke. Most of India's coal comes from Jharia. Jharia coal mines are India's most important storehouse of prime coke coal used in blast furnaces, it consists of 23 large underground and nine large open cast mines.",
+    Location: "Jharia, Jharkhand",
+    background:
+      "Jharia coalfield is a large coal field located in the east of India in Jharia, Jharkhand. Jharia represents the largest coal reserves in India having estimated reserves of 19.4 billion tonnes of coking coal. The coalfield is an important contributor to the local economy, employing much of the local population either directly or indirectly. The fields have suffered a coal bed fire since at least 1916, resulting in 37 millions tons of coal consumed by the fire, and significant ground subsidence and water and air pollution in local communities including the city of Jharia. The resulting pollution has led to a government agency designated for moving local populations, however, little progress has been made in the relocation.",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Coal",
+    Grade: "Bituminous Coal",
+    "Mine Type": "20 underground mines and several large open-cast coal mines",
+    "Total Reserve": "19.4 billion tonnes",
+    Usage: "Coal obtained is used as coke coal",
+    Company: "Bharat Cooking Coal Limited",
+    "Mining Since": "1894",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/Jharia_coalfield",
+      "India's Jharia coal field has been burning for 100 years - CNBC":
+        "https://www.cnbc.com/2015/12/02/indias-jharia-coal-field-has-been-burning-for-100-years.html",
+      Britannica: "https://www.britannica.com/place/Jharia",
+    },
+  },
+  {
+    id: 004,
+    Name: "West Bokaro Coalfield",
+    lat: 23.8,
+    long: 85.75,
+    geoDetail:
+      "West Bokaro Coalfield is located mostly in Ramgarh district and partly in Hazaribagh district in the Indian state of Jharkhand. The Bokaro coalfield lies between 23° 45’ and 23° 50’ North latitude and 85° 30’ and 86° 03’ East longitude. It spreads 65 km from east to west and 10 to 16 km from north to south. Bokaro West and Bokaro East are two subdivisions of the field separated almost in the middle by Lugu Hill (height 960.9 m (3,153 ft))Bokaro River passes through the West Bokaro and East Bokaro coalfields. West Bokaro Coalfield covers an area of 259 square kilometres (100 sq mi) and has total coal reserves of 4,246.30 million tonnes.",
+    Location: "Ramgarh district, Hazaribagh district, Jharkhand.",
+    background:
+      "In 1917, L.S.S.O’Malley described the coalfields in the upper reaches of the Damodar as follows: 'Near the western boundary of Jharia field is that of Bokaro, covering 220 square miles (570 km2), with an estimated content of 1,500 million tons; close by… is the Ramgarh field (40 square miles), in which, however, coal is believed to be of inferior quality. A still larger field in the same district is that called Karanpura, which extends over 544 square miles (1,410 km2) and has an estimated capacity of 9,000 million tons.'",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Coal",
+    Grade: "Bitumious",
+    "Total Reserve": "5012.49 Million Tonnes (as on 1/4/2010)",
+    "Mine Type": "Open Cast",
+    Usage: "used as coke coal",
+    Company: "Tata Steel",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/West_Bokaro_Coalfield",
+    },
+  },
+  {
+    id: 005,
+    Name: "East Bokaro Coalfield",
+    lat: 23.8,
+    long: 85.75,
+    geoDetail:
+      "West Bokaro Coalfield is located mostly in Ramgarh district and partly in Hazaribagh district in the Indian state of Jharkhand.The Bokaro coalfield lies between 23° 45’ and 23° 50’ North latitude and 85° 30’ and 86° 03’ East longitude. It spreads 65 km from east to west and 10 to 16 km from north to south. Bokaro West and Bokaro East are two subdivisions of the field separated almost in the middle by Lugu Hill (height 960.9 m (3,153 ft))Bokaro River passes through the West Bokaro and East Bokaro coalfields. West Bokaro Coalfield covers an area of 259 square kilometres (100 sq mi) and has total coal reserves of 4,246.30 million tonnes.",
+    Location: "Ramgarh district, Hazaribagh district, Jharkhand.",
+    background:
+      "In 1917, L.S.S.O’Malley described the coalfields in the upper reaches of the Damodar as follows: 'Near the western boundary of Jharia field is that of Bokaro, covering 220 square miles (570 km2), with an estimated content of 1,500 million tons; close by… is the Ramgarh field (40 square miles), in which, however, coal is believed to be of inferior quality. A still larger field in the same district is that called Karanpura, which extends over 544 square miles (1,410 km2) and has an estimated capacity of 9,000 million tons.'",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Coal",
+    Grade: "Bitumious",
+    "Total Reserve": "8083.29 Million Tonnes (as on 1/4/2010)",
+    "Mine Type": "Open Cast",
+    Usage: "used as coke coal",
+    Company: "Tata Steel",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/East_Bokaro_Coalfield",
+    },
+  },
+  {
+    id: 006,
+    Name: "Rajmahal coalfield",
+    lat: 25.2,
+    long: 87.2,
+    geoDetail:
+      "The Rajmahal Coalfield is a large coal field located in the east of India in Jharkhand.",
+    Location: "Mahagama village, Godda district, Jharkhand state, India",
+    background:
+      "Five relatively small coal basins – Hura, Chuperbhita, Pachwara, Mahuagarhi and Brahmani - compose the Rajmahal Coalfields. They form a sort of a broken chain along the western flank of the Rajmahal Hills in the north to West Bengal's Birbhum district in the south. The main advantage of this coalfield is that much of its reserves are untouched. Another description shows the Rajmahal Area as being composed of the following: Hura, Gilhuria and Jilbari, Chuparbhita, Pachwara and Brahmani.",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Coal",
+    Grade: "Superior quality non-coking coal",
+    "Production Capacity": "17 million tonnes",
+    "Total Reserve": "13.13 billion tonnes",
+    "Mine Type": "Open Cast",
+    "Amount Of Ore Mined":
+      "Expected to produce 200 million tonnes of coal over 12.5 years.",
+    Usage:
+      "supplies coal to the 2,100 MW Farakka Super Thermal Power Station and the 2,340 MW Kahalgaon Super Thermal Power Station.",
+    Sponser:
+      " Essel Mining and Industries Limited (EMIL) of the Aditya Birla Group",
+    "Commissioned Date": "1980s",
+    Company: "Eastern Coalfields Limited (ESL)",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/Rajmahal_coalfield",
+    },
+  },
+  {
+    id: 007,
+    Name: "North Karanpura Coalfield",
+    lat: 23.685074,
+    long: 84.989074,
+    geoDetail:
+      "North Karanpura Coalfield is located in Ranchi, Hazaribagh, Chatra and Latehar districts in the Indian state of Jharkhand. The location of the North Karanpura coalfield, which is spread across the districts of Hazaribagh, Ranchi, Chatra, and Latehar, in the Karanpura Valley of Jharkhand state. The field stretches 64 km east-to-west and 32 km north-to-south, and covers 1,230 square kilometers.",
+    Location:
+      "Districts of Ranchi, Hazaribagh, Chatra and Latehar districts in the Indian state of Jharkhand.",
+    background:
+      "The North Karanpura coal mines are a network of underground and opencast coal mines, operated by Central Coalfields Limited, a subsidiary of Coal India, producing more than 20 million-tonnes-per-annum, on the North Karanpura coalfield in Jharkhand state, India. The operating mines include: Churi, Dakra Bukbuka, K. D. H., Rohini Karkatta, Rohini, and Purnadih",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Coal",
+    "Production Capacity": "20 million-tonnes-per-annum",
+    "Total Reserve": "17,073.24 billion tonnes",
+    "Mine Type": "Underground and Open Cast coal mines",
+    "Mine Depth": "65m",
+    Owner: "Central Coalfields Limited",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/North_Karanpura_Coalfield",
+    },
+  },
+  {
+    id: 008,
+    Name: "Raniganj Coalfield",
+    lat: 23.61672,
+    long: 87.13333,
+    geoDetail:
+      "Raniganj Coalfield is primarily located in the Asansol and Durgapur subdivisions of Paschim Bardhaman district of West Bengal. It spreads over to the neighboring districts of Birbhum, Bankura, Purulia and to Dhanbad district of Jharkhand. Raniganj Coalfield covers an area of 443.50 km2 (171.24 sq mi) and has total coal reserves of 49.17 billion tonnes, spread across Indian states of West Bengal and Jharkhand. That makes it the second largest coalfield in the country (in terms of reserves).",
+    Location: "Various districts in West Bengal and Jharkhand States, India",
+    background:
+      "Coalmining in India first started in the Raniganj Coalfield. In 1774, John Sumner and Suetonius Grant Heatly of the British East India Company found coal near Ethora, presently in Salanpur community development block. The early exploration and mining operations were carried out in a haphazard manner.For the entire 19th century and a major part of the 20th century, Ranigunj coalfields was the major producer of coal in the country.All non-coking coal mines were nationalized in 1973 and placed under Coal Mines Authority of India. In 1975, Eastern Coalfields Limited, a subsidiary of Coal India Limited, was formed. It took over all the earlier private collieries in Raniganj Coalfield.",
+    "Current Status": "Operating",
+    photos: "",
+    "Mineral Mined": "Coal",
+    "Production Capacity": "33.90 million tonnes (as in 2012-2013)",
+    "Total Reserve":
+      "30.61 billion tonnes is in the West Bengal and 18.56 billion tonnes is in Jharkhand.",
+    "Mine Type":
+      "77 Underground Mines and 21 Open Cast Mines (as in 2012-2013)",
+    Manpower: "72,973 (as in 2012-2013)",
+    "In Service": "1774, 1973",
+    Company:
+      "Eastern Coalfields Limited (ECL) which is a subsidary of Coal India",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/Raniganj_Coalfield",
+    },
+  },
+  {
+    id: 009,
+    Name: "Birbhum Coalfield",
+    lat: 24.05916,
+    long: 87.59965,
+    geoDetail:
+      "The Birbhum is a large coal field located in the east of India in West Bengal. Birbhum is having estimated reserves of 5 billion tonnes of coal. Deucha Pachami coal block spread over an area of 9.7 km2 is to be developed by Bengal Birbhum Coal Company Limited. Another potential coal reserve is Dewanganj Harinsingha block with an estimated area of 2.6 km2. These blocks are located in Mohammad Bazar CD Block.",
+    Location: "Deucha village, Birbhum District, West Bengal State, India",
+    background:
+      "The Deocha-Pachami and Dewanganj-Harinsingha coal blocks lie adjacent to one another on the Birbhum coalfield, over an area of 12.3 square kilometers. With indicated reserves of 40.7 billion tons, this coalfield is one of India's largest. India's Ministry of Coal is currently in the process of allocating these coal blocks (as a single block) to an as-yet-undetermined state-owned company. While the Ministry stated in June 2013 that it hoped to allocate this block to a state company 'within a week,' that process is apparently still ongoing as of May 2015. The initial investment is expected to be around 100 billion rupees, or about $1.5 billion.",
+    "Current Status": "Proposed",
+    photos: "",
+    "Mineral Mined": "Coal",
+    "Total Reserve": "5 Billions Tonnes (estimated)",
+    url: {
+      Wikipedia: "https://en.wikipedia.org/wiki/Birbhum_Coalfield",
     },
   },
 ];
